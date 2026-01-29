9e697dbb5aea1ae49e634df4eebef5c91133f910
nkl <spinpo12@naver.com>
2026-01-29 10:05:15 +0900

    wasm-pack 이 자동으로 스니펫을 생성하기에 복사로직 제거

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ echo Building WASM...
 wasm-pack build --target web
 
 echo Copying JS assets to pkg...
-copy /Y src\editor\editor_support.js pkg\
+::copy /Y src\editor\editor_support.js pkg\
 
 echo Done!
